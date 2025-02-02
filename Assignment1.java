ac4d5574c11a1c2ff51bf69e64be4eaf6e49d61d
creatoraswin <kumarikumar235@gmail.com>
2025-02-02 19:11:17 +0530

    Assignment1

---
+++ Assignment1.java
@@ -0,0 +1,91 @@
+public class Assignment1 {
+
+    public void printLetterI() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 5 || j == 3)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printLetterN() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (j == 1 || j == 5 || (i == j))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printLetterE() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 3 || i == 5 || j == 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printLetterU() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (j == 1 || j == 5 || i == 5)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printLetterR() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 3 || j == 1 || (i == 2 && j == 5) || (i == 4 && j == 3) || (i == 5 && j == 4))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printLetterO() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 5 || j == 1 || j == 5)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Assignment1 printer = new Assignment1();
+        printer.printLetterI();
+        System.out.println();
+        printer.printLetterN();
+        System.out.println();
+        printer.printLetterE();
+        System.out.println();
+        printer.printLetterU();
+        System.out.println();
+        printer.printLetterR();
+        System.out.println();
+        printer.printLetterO();
+        System.out.println();
+        printer.printLetterN();
+    }
+}
